66ba64a0fc93a403527e62115ca6b6700c1b5568
agarridogarcia <agarridogarcia@atanasoff30.danielcastelao.org>
2016-10-03 09:28:59 +0200

    inicio boletin

---
+++ src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,26 @@
+
+package boletin2_1;
+
+import javax.swing.JOptionPane;
+
+
+public class Boletin2_1 {
+
+  
+    public static void main(String[] args) {
+        
+        float base, altura, area;
+        
+      base=Float.parseFloat(JOptionPane.showInputDialog(" base"));
+      altura=Float.parseFloat(JOptionPane.showInputDialog("altura"));
+      area=base*altura;
+      
+      
+      //System.out.println("area = "+area);
+        JOptionPane.showMessageDialog(null, "area="+area);
+        
+        
+        
+    }
+    
+}
